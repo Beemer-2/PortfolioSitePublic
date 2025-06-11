229bff48e93e5d522973c013613e7059c56b628b
Beemer-2 <151047145+Beemer-2@users.noreply.github.com>
2025-06-11 13:33:37 +0100

    Added titles and started working on instant display

---
+++ javascript/instant-display.js
@@ -0,0 +1,10 @@
+"use strict"
+
+
+
+for (let hexagon of document.querySelectorAll(".hexagon")) {
+    let image = hexagon.firstElementChild;
+    image.addEventListener("mouseover", (e) => {
+        
+    });
+}
